74fdec0596df7eb0be432d05ee075574b7e76811
Tim Popham <popham@uw.edu>
2019-02-08 00:01:45 -0800

    Don't export generic pointer encoders

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@
 export { default as cap } from "./cap";
 export * from "./far";
 export * from "./list";
-export { default as nonpreallocated } from "./nonpreallocated";
-export { default as preallocated } from "./preallocated";
 export * from "./struct";
