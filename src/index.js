f3bff6e3b935145aa4c10260e73038189e58a5f1
Tim Popham <popham@uw.edu>
2019-02-11 01:34:40 -0800

    Export generic pointer encoders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /* @flow */
 
 export { default as cap } from "./cap";
+export { default as nonpreallocated } from "./nonpreallocated";
+export { default as preallocated } from "./preallocated";
 export * from "./far";
 export * from "./list";
 export * from "./struct";
